418912df9924b5723899dd5e6c892f6a88633a00
newick <erick@entre-quote.com>
2016-04-26 17:06:00 +0200

    added tools on viewer

--- src/layout-viewer.js
+++ src/layout-viewer.js
@@ -6,8 +6,17 @@
   layout.create('viewer')
   layout.viewer.html(`
   <main>
-    <div id="parentRepo" class="breadcrumbs" data-template="parentRepo">
+    <div id="parentRepo" class="parentRepo" data-template="parentRepo">
     </div>
+    <aside class="viewer-actions">
+      <nav>
+        <ul>
+          <li><a href="" title="Editer sur prose.io"><img src="../img/font-selection-editor.svg" alt="Editer sur prose.io"></a></li>
+          <li><a href="" title="Besoin d'aide ?"><img src="../img/question-sign.svg" alt="Besoin d'aide ?"></a></li>
+        </ul>
+      </nav>
+      <a href="" title="Remonter en haut de la page"><img src="../img/arrow-up.svg" alt="Remonter en haut de la pge"></a>
+    </aside>
     <article data-template="contribution" id="contribution">
     </article>
   </main>
